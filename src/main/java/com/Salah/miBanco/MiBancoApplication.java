ab7485b7853ad5112f7a536647a1b262bbc7fceb
Salah eddine khouadri <salahidslhddnn@gmail.com>
2025-01-25 23:48:19 +0100

    1st Commit

---
+++ src/main/java/com/Salah/miBanco/MiBancoApplication.java
@@ -0,0 +1,13 @@
+package com.Salah.miBanco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiBancoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiBancoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Salah/miBanco/MiBancoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Salah.miBanco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiBancoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
