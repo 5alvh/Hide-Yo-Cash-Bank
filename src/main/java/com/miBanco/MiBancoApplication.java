5d92be67a8adfbca6d6e4bf216ddbda8f578d2b4
Salah eddine khouadri <salahidslhddnn@gmail.com>
2025-02-05 23:59:03 +0100

    Controllers Created

--- src/main/java/com/miBanco/MiBancoApplication.java
+++ src/main/java/com/miBanco/MiBancoApplication.java
@@ -1,4 +1,4 @@
-package com.Salah.miBanco;
+package com.miBanco;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/miBanco/MiBancoApplicationTests.java
+++ src/test/java/com/miBanco/MiBancoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.Salah.miBanco;
+package com.miBanco;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
