c18c210938f213cf9589ba415bd79fbc69ff49d3
Salah eddine khouadri <salahidslhddnn@gmail.com>
2025-03-25 16:28:25 +0100

    commit

---
+++ src/main/java/com/miBanco/config/MyBankUserService.java
@@ -0,0 +1,29 @@
+package com.miBanco.config;
+
+import com.miBanco.model.Customer;
+import com.miBanco.repository.CustomerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class MyBankUserService implements UserDetailsService {
+
+    private final CustomerRepository customerRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Customer customer = customerRepository.findByEmail(username)
+                .orElseThrow(()-> new UsernameNotFoundException("User "+username+" not found") );
+        List<GrantedAuthority> authorities = List.of(new SimpleGrantedAuthority(customer.getRole()));
+        return new User(customer.getEmail(), customer.getPwd(), authorities);
+    }
+}
--- src/main/java/com/miBanco/config/ProjectSecurityProdConfig.java
+++ src/main/java/com/miBanco/config/ProjectSecurityProdConfig.java
@@ -3,38 +3,43 @@ package com.miBanco.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
-@Profile("!prod")
-public class ProjectSecurityConfig {
+@Profile("prod")
+public class ProjectSecurityProdConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        /*http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());*/
-        /*http.authorizeHttpRequests((requests) -> requests.anyRequest().denyAll());*/
-        /*http.authorizeHttpRequests((requests) -> requests.anyRequest().permitAll());*/
-        http.requiresChannel((requests) -> requests.anyRequest().requiresInsecure())
-        .csrf(csrfConfig -> csrfConfig.disable());
-        http.authorizeHttpRequests(
-                (requests)->
-                        requests.requestMatchers("/myAccount", "/myBalance", "/notices","/myCards", "/myLoans").authenticated()
-                        .requestMatchers("/contact","/error","/register").permitAll()
-
-                );
+        http.requiresChannel((requests) -> requests.anyRequest().requiresSecure())
+        .csrf(csrfConfig -> csrfConfig.disable())
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards").authenticated()
+                        .requestMatchers("/notices", "/contact", "/error", "/register").permitAll());
         http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
         return http.build();
     }
 
-
     @Bean
     public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
+
+    /**
+     * From Spring Security 6.3 version
+     * @return
+     */
+    @Bean
+    public CompromisedPasswordChecker compromisedPasswordChecker() {
+        return new HaveIBeenPwnedRestApiPasswordChecker();
+    }
+
 }
